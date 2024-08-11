d6432456f422f8e4f46f3bd9aca6b98cd7bd7399
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2024-08-12 03:47:26 +0530

    Customer Data Analysis: JSON formatter script

---
+++ json_formatter.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+if __name__ == "__main__":
+    data_path = r"../Customer Data Analysis/"
+
+    # portfolio
+    portfolio_df = pd.read_csv(data_path + "portfolio.csv", encoding="utf-8")
+    portfolio_df["channels"] = portfolio_df["channels"].apply(
+        lambda x: x.replace(r"'", r'"'))
+    portfolio_df.to_csv(data_path + "portfolio_processed.csv",
+                        encoding="utf-8",
+                        index=None)
+
+    # transcript
+    transcript_df = pd.read_csv(data_path + "transcript.csv", encoding="utf-8")
+    transcript_df["value"] = transcript_df["value"].apply(
+        lambda x: x.replace(r"'", r'"'))
+    transcript_df.to_csv(data_path + "transcript_processed.csv",
+                         encoding="utf-8",
+                         index=None)
